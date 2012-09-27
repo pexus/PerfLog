1fbcd3f3b3c912cf89317f16227a1943be0f703e
Pradeep <pradeep@win7x64-PC>
2012-09-27 15:21:09 -0500

    fix header

--- PerfLogAppLogger/src/org/perf/log/app/logger/AppLogMessageFormatter.java
+++ PerfLogAppLogger/src/org/perf/log/app/logger/AppLogMessageFormatter.java
@@ -23,23 +23,6 @@ import java.text.MessageFormat;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-/*******************************************************************************
- * Copyright 2012 Pradeep Nambiar,  Pexus LLC
- * 
- * Source File: src/org/perf/log/app/logger/AppLogMessageFormatter.java 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 /**
  * This class is used to format the message as passed.
  * MessageFormat can be edited to add any prefix or suffix to log messages.
